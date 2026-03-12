c59e7898b4639e04f53e7b6593cda9d2dac79c04
andriskiraj <andrisb0829@gmail.com>
2026-03-12 11:49:08 +0100

    mittomen

--- autok/MainWindow.xaml.cs
+++ autok/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -16,28 +18,66 @@ namespace autok
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] sorok;
         public MainWindow()
         {
             InitializeComponent();
+            sorok = File.ReadAllLines("autok.txt");
+
+            foreach (string sor in sorok)
+            {
+                string[] adatok = sor.Split(';');
+                textbox.AppendText(adatok[0] + "\n");
+            }
+
         }
 
-        private void uj_Click(object sender, RoutedEventArgs e)
+        private void uj_Click_1(object sender, RoutedEventArgs e)
         {
             UjAuto ujAuto = new UjAuto();
             ujAuto.Show();
             this.Close();
         }
 
-        private void bezar_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void modositas_Click(object sender, RoutedEventArgs e)
+        private void modositas_Click_1(object sender, RoutedEventArgs e)
         {
             AutoModositas modositas = new AutoModositas();
             modositas.Show();
             this.Close();
+        }
+
+        private void torles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bezar_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void rendszamm(object sender, RoutedEventArgs e)
+        {
+            string rendszam = textbox.SelectedText.Trim();
+
+            foreach (string sor in sorok)
+            {
+                string[] adatok = sor.Split(';');
+
+                if (adatok[0] == rendszam)
+                {
+                    uzemanyag.Content = adatok[1];
+                    km.Content = adatok[2];
+                    szin.Content = adatok[3];
+                    tipus.Content = adatok[4];
+                    
+                }
+
+            }
+            if( rendszam =)
+            {
+
+            }
+            
         }
     }
 }
